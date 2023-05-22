597232e4bb5619f479d6fd4eb131a420aa017165
samuvel anbudavid <sam.a.bca@gmail.com>
2023-05-23 00:57:03 +0530

    complete

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 require('dotenv').config();
 var express = require('express');
 var app = express();
+const IP = require('ip');
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
@@ -24,6 +25,10 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', function (req, res) {
+  const ipAddress = IP.address();
+  res.json({"ipaddress":ipAddress,"language":req.headers['accept-language'],"software":req.headers['user-agent']});
+});
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
